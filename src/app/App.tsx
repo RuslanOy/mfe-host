5157d0559bfc0b6e98241513c8aa6bc1f4a7bbbd
Ruslan Oyun <careerRuslanOyun@yandex.ru>
2025-06-06 23:23:55 +0600

    add skeleton

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import React, { Suspense } from 'react';
+import { Box, Flex, Skeleton } from '@chakra-ui/react';
 import { Header } from '@/features/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from '@/pages/Home';
@@ -12,13 +12,15 @@ export const App = (): React.ReactElement => {
     <BrowserRouter>
       <Flex direction="column" alignItems="center">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutMe />} />
-          <Route path="/todos" element={<ToDoList />} />
-          <Route path="/wallet" element={<Box>Wallet Page (Coming Soon)</Box>} />
-          <Route path="/workers" element={<Box>Web Workers Page (Coming Soon)</Box>} />
-        </Routes>
+        <Suspense fallback={<Skeleton height="100vh" width="700px" />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/todos" element={<ToDoList />} />
+            <Route path="/wallet" element={<Box>Wallet Page (Coming Soon)</Box>} />
+            <Route path="/workers" element={<Box>Web Workers Page (Coming Soon)</Box>} />
+          </Routes>
+        </Suspense>
       </Flex>
     </BrowserRouter>
   );
