9f587c33ce0a0e080286d2aa334c78a6f89b4e1e
Ruslan Oyun <careerRuslanOyun@yandex.ru>
2025-07-17 02:14:34 +0600

    hide two pages

--- src/features/Header/constants.ts
+++ src/features/Header/constants.ts
@@ -6,6 +6,6 @@ export interface HeaderLink {
 export const headerLinks: HeaderLink[] = [
   { label: 'Home', path: '/' },
   { label: 'ToDo List', path: '/todos' },
-  { label: 'Crypto', path: '/crypto' },
-  { label: 'Web Workers', path: '/workers' },
+  // { label: 'Crypto', path: '/crypto' },
+  // { label: 'Web Workers', path: '/workers' },
 ];
